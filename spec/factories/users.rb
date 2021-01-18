0b9c85b1f51b1a7299fe7c52af42c8e7a936ce3a
yumi-jpn <yumiruhosi1@gmail.com>
2021-01-18 11:51:49 +0900

    userモデル単体テスト　修正3

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     end
     nickname               { Faker::Name.initials(number: 2) }
     email                  { Faker::Internet.free_email }
-    password               { Faker::Internet.password(min_length: 6) }
+    password               { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation  { password }
     last_name              { person.first.kanji }
     first_name             { person.last.kanji }
