bc954a0fc7c178ca6fe994d483289722bcf8970f
yumi-jpn <yumiruhosi1@gmail.com>
2021-01-21 17:55:14 +0900

    itemモデル単体テスト　修正 6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, only: [:new, :create]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def index
