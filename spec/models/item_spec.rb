6da2b2f86eefa58df02aa387a7739cc58602d6cb
yumi-jpn <yumiruhosi1@gmail.com>
2021-01-21 17:44:10 +0900

    itemモデル単体テスト　修正 5

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -78,7 +78,7 @@ RSpec.describe Item, type: :model do
           @item.valid?
           expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
         end
-        it 'priceが¥9,999,999以上だと登録できない' do
+        it 'priceが¥10,000,000以上だと登録できない' do
           @item.price = '100000000'
           @item.valid?
           expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
