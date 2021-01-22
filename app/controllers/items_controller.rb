ca459580090d9a6eec339126af531d9107ef6be9
yumi-jpn <yumiruhosi1@gmail.com>
2021-01-22 21:25:10 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :ensure_user, only: [:edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -7,10 +8,6 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
-  end
-
-  def show
-    @item = Item.find(params[:id])
   end
 
   def create
@@ -22,10 +19,34 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.user_id == current_user.id
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :item_name, :explanation, :category_id, :state_id, :ship_method_id, :ship_area_id,
                                  :ship_date_id, :price, user_ids: []).merge(user_id: current_user.id)
+  end
+
+  def ensure_user
+    @items = current_user.items
+    @item = @items.find_by(id: params[:id])
+    redirect_to root_path unless @item
   end
 end
