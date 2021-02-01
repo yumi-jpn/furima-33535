b5c025142661e0ae6565a26ef5eaf51333552d74
yumi-jpn <yumiruhosi1@gmail.com>
2021-02-01 12:38:11 +0900

    商品購入機能　修正2

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items do
-    resources :item_users, only: [:index, :new, :create]
+    resources :item_users, only: [:index, :create]
   end
 end
